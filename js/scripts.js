59a75fd4a32f1e620843905280b9823258317463
Courtney Yatteau <courtney.yatteau@gmail.com>
2022-02-24 20:17:06 -0500

    Added a List of Three Pokémon Objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
-alert("Hello world");
-let favoriteFood = "spaghetti";
-document.write(favoriteFood);
+let pokemonList = [
+  {
+    name: "Weedle",
+    number: 13,
+    height: 1.0,
+    types: ["Bug", "Poison"],
+    weaknesses: ["Fire", "Psychic", "Flying", "Rock"],
+  },
+  {
+    name: "Jigglypuff",
+    number: 39,
+    height: 1 + 2 / 3,
+    types: ["Normal", "Fairy"],
+    weaknesses: ["Steel", "Poison"],
+  },
+  {
+    name: "Poliwag",
+    number: 60,
+    height: 2.0,
+    types: ["Water"],
+    weaknesses: ["Grass", "Electric"],
+  },
+];
+
+for (i = 0; i < pokemonList.length; i++) {
+  document.write(`<div>${pokemonList[i].name}</div>`);
+}
