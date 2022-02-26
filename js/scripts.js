798671fab19525374b1210b1d8456b11576ca927
Courtney Yatteau <courtney.yatteau@gmail.com>
2022-02-26 14:25:41 -0500

    Task 1.5 - for now forEach and IIFE wrap

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,51 @@
 //list of pokemon
-let pokemonList = [
-  {
-    name: "Weedle",
-    number: 13,
-    height: 1.0,
-    types: ["Bug", "Poison"],
-    weaknesses: ["Fire", "Psychic", "Flying", "Rock"],
-  },
-  {
-    name: "Jigglypuff",
-    number: 39,
-    height: 1 + 2 / 3,
-    types: ["Normal", "Fairy"],
-    weaknesses: ["Steel", "Poison"],
-  },
-  {
-    name: "Poliwag",
-    number: 60,
-    height: 2.0,
-    types: ["Water"],
-    weaknesses: ["Grass", "Electric"],
-  },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "Weedle",
+      number: 13,
+      height: 1.0,
+      types: ["Bug", "Poison"],
+      weaknesses: ["Fire", "Psychic", "Flying", "Rock"],
+    },
+    {
+      name: "Jigglypuff",
+      number: 39,
+      height: 1 + 2 / 3,
+      types: ["Normal", "Fairy"],
+      weaknesses: ["Steel", "Poison"],
+    },
+    {
+      name: "Poliwag",
+      number: 60,
+      height: 2.0,
+      types: ["Water"],
+      weaknesses: ["Grass", "Electric"],
+    },
+  ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
 
 //loops through the pokemon list
-for (let i = 0; i < pokemonList.length; i++) {
-  let statement = "";
+let pokemonList = pokemonRepository.getAll();
+pokemonList.forEach(function (pokemon) {
   //checks if height is at least 2. If so, states it's big!
-  if (`${pokemonList[i].height}` >= 2) {
-    statement = "- Wow, that's big!";
-  } else {
-    statement = "";
-  }
+  let statement = "";
+  pokemon.height >= 2 ? (statement = "- Wow, that's big!") : (statement = "");
   //shows pokemon name and height each on their own row
   document.write(
-    `<div>${pokemonList[i].name} (height: ${pokemonList[i].height}) ${statement}</div>`
+    `<div>${pokemon.name} (height: ${pokemon.height}) ${statement}</div>`
   );
-}
+});
